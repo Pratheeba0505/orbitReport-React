5971d02017a8acc0469e5d170a151a4c36c43929
Pratheeba0505 <pratheeba0505@gmail.com>
2024-03-20 11:17:28 -0500

    OrbitReport- React
    OrbitReport- React

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,10 +1,26 @@
-const Buttons = ({ filterByType, setSat, displaySats }) => {
-  return (
-    <div>
-      <button>Placeholder Button</button>
-      <button>All Orbits</button>
-    </div>
-  );
+//import React  from 'react';
+import satData from "./satData";
+import './styling.css'
+
+const Buttons =  ({ filterByType, setSat, displaySats})=>{
+  //let filterByType = ""
+  //let setSat = ""
+  //let displaySats = "";
+
+  let button = displaySats.map((sat, id) => {
+    return (
+      <button onClick={() => filterByType(sat)} key={id}>
+        {sat} Orbit
+      </button>
+    );
+  });
+  let button1 = <div>
+    {button}
+      <button onClick={() => setSat(satData)}>All Orbits</button>
+      </div>
+      return button1;
+
+ 
 };
 
 export default Buttons;
--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,18 +1,36 @@
 const Table = ({ sat }) => {
+  let tableBody = sat.map((data,id) =>{
+    const status = data.operational ? "Active" : "Inactive";
+  
   return (
-    <table>
-    <thead>
-     <tr>
-       <th>Header TBD</th>
-     </tr>
-     </thead>
-     <tbody>
-     <tr>
-       <td>Row Data TBD</td>
-     </tr>
-     </tbody>
-   </table>
+    <tbody>
+        <tr key = {id}>
+        <td>{data.name}</td>
+        <td>{data.type}</td>
+        <td>{data.launchDate}</td>
+        <td>{status}</td>
+        </tr>
+    </tbody>
+
   );
+  })
+
+
+
+  let table=  
+  <table>
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Type of Satellite</th>
+        <th>Launch Date</th>
+        <th>Status</th>
+      </tr>
+      </thead>
+      {tableBody}
+    </table>
+    return table
 };
+
 
 export default Table;
